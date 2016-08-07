5e76ab63bae8a1065d3c935c16ab9b21de33c419
viniyata <viniyata@gmail.com>
2016-08-07 12:18:23 -0400

    added show hide feature

--- js/app.js
+++ js/app.js
@@ -5,60 +5,44 @@ var textures = ["http://itp.viniyatapany.com/nature-of-code/midTerm-05/",
                      "http://itp.viniyatapany.com/nature-of-code/midTerm-05/",
                      "http://itp.viniyatapany.com/nature-of-code/midTerm-05/"]; //initialize array of textures
 
-var soundStories = `In a subway station full of people. No one is talking.
-Only sounds of someone coughing
-Feet dragging across the tiles on the floor
-There is an announcement – It is Muffled
-People breathing heavily
-The train station air vent fan
-The guy in the green coat yawns
-The sound of a someone’s hand rubbing off his black jacket as he fixes his headphones
-He sneezed
-A zipper is opened
-Someone is folding papers
-I can hear a train
-Train doors open
-Train’s door closes – ding ding
-More dragging footsteps
-No one has spoken yet
-Another train going the other direction I can see two people talking but they are far away – I can’t hear them`; //make array to add the soundStories
+var soundStories = ["0 ng the other direction I can see two people talking but they are far away – I can’t hear them",
+                    "1 ng the other direction I can see two people talking but they are far away – I can’t hear them",
+                    "2 ng the other direction I can see two people talking but they are far away – I can’t hear them",
+                    "3 ng the other direction I can see two people talking but they are far away – I can’t hear them",
+                    "4 ng the other direction I can see two people talking but they are far away – I can’t hear them",
+                    "5 ng the other direction I can see two people talking but they are far away – I can’t hear them"]
+ //make array to add the soundStories
 
 
 function init(){
   for (var i = 0; i < textures.length; i++) { //for loop to add multiple textues (array) on the page
-    var iframeContainer = document.createElement("div"); // making a div via createElement because its am html element
+    var container = document.createElement("div"); // making a div via createElement because its am html element
     var iframe = document.createElement("iframe"); // making an iframe via createElement because its am html element
     iframe.setAttribute("src", textures[i]);
     iframe.setAttribute("class", "texture__iframe"); //BEM : Block
-    iframeContainer.setAttribute("class", "texture__container"); //BEM : http://getbem.com/naming/
+    container.setAttribute("class", "texture__container"); //BEM : http://getbem.com/naming/
 
     var changeState = document.createElement("button");
     changeState.setAttribute("class", "texture__button");
-    changeState.setAttribute("data-number", i );
-    changeState.setAttribute("onclick", "test(this.getAttribute('data-number'))" );
+    changeState.setAttribute("data-number", i ); //data attribute
+    changeState.setAttribute("onclick", "showstory(this.getAttribute('data-number'))" );
 
-    iframeContainer.appendChild(changeState);
+    container.appendChild(changeState);
 
-    iframeContainer.appendChild(iframe);
-    document.body.appendChild(iframeContainer);
-
-    var content = document.createElement("p");
-    content.innerHTML = soundStories; //check why order matters
-    content.setAttribute("class", "texture__content");
-    var tempId = "texture__content-" + i;
-    content.setAttribute("id", tempId);
-    iframeContainer.appendChild(content);
+    container.appendChild(iframe);
+    document.body.appendChild(container);
 
   }
 }
 
-function test(no) {
+function showstory(no) {
   console.log(no);
-  var idNumber = "texture__content-" + no;
-  if (document.getElementById(idNumber).style.display == "block"){
-    document.getElementById(idNumber).style.display = "none";
+  var showstoryElement = "showstory__textid";
+  if (document.getElementById(showstoryElement).style.display == "block"){
+    document.getElementById(showstoryElement).style.display = "none";
   } else {
-    document.getElementById(idNumber).style.display = "block";
+    document.getElementById(showstoryElement).style.display = "block";
+    document.getElementById(showstoryElement).innerHTML = soundStories[no];
   }
 }
 
